8a73af30ef4121040cf6f7a057e508e6a0d8cb49
MoMo LaFrOOO <62650527+momolafrooo@users.noreply.github.com>
2022-08-03 11:54:27 +0000

    ✅validation on login

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Finelunch')
+    .setDescription('Finelunch API description')
+    .setVersion('1.0')
+    .addTag('Finelunch')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
